3141a0fa0af0e0ee53b3c7f00c84d918c2fb4659
myelin0 <emi.veisllari@gmail.com>
2022-03-18 10:14:09 +0100

    Add and remove books functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Book = (props) => {
-  const { title, author } = props;
+const Book = ({
+  id, title, author, category, removeBookHandler,
+}) => {
+  const [percentage, setPercentage] = useState(Math.floor(Math.random() * 99));
+  const Progress = () => {
+    setPercentage((prevPercent) => {
+      if (prevPercent < 100) {
+        if ((prevPercent + 10) <= 100) {
+          return prevPercent + 10;
+        }
+        const final = 100 - prevPercent;
+        return prevPercent + final;
+      }
+      return prevPercent;
+    });
+  };
+
+  const capitalizeFirstChar = (str) => str.charAt(0).toUpperCase() + str.slice(1);
   return (
     <div>
-      <p className="bookCategory">Action</p>
+      <p className="bookCategory">{capitalizeFirstChar(category)}</p>
       <h2 className="BookTitle">{title}</h2>
       <span className="author">{author}</span>
       {' '}
       <br />
       <button type="button">Comments</button>
-      <button type="button">
+      <button
+        type="button"
+        onClick={() => removeBookHandler(id)}
+      >
         Remove
       </button>
       <button type="button">Edit</button>
@@ -22,16 +41,24 @@ const Book = (props) => {
         Chapter
         {' '}
         {' '}
-        17%
+        {percentage}
       </p>
-      <button type="button">
+      <button
+        type="button"
+        onClick={Progress}
+      >
         UPDATE PROGRESS
       </button>
     </div>
   );
 };
+
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  removeBookHandler: PropTypes.func.isRequired,
 };
+
 export default Book;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 import NavBar from './components/NavBar';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/Categories" element={<Categories />} />
-    </Routes>
-  </Router>,
+  <Provider store={store}>
+    <Router basename={process.env.PUBLIC_URL}>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
